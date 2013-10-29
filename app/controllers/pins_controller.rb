16cdf977640832e75e5f33d50fbec709dac43750
Philip Q Nguyen <supertaru@gmail.com>
2013-10-29 15:17:06 +0900

    Add related_paymentaccount in pins_controller under show. Modified view Show accordingly

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,6 +20,9 @@ class PinsController < ApplicationController
   def show
     @pin = Pin.find(params[:id])
     @is_admin = current_user && current_user.id == @pin.user_id
+    @related_paymentaccount = Paymentaccount.find_by_user_id(@pin.user_id)
+
+
 
     respond_to do |format|
       format.html # show.html.erb
